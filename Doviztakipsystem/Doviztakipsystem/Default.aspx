919126110417278d82c95a9f88812205845d92f5
PC <PC@PC-BILGISAYAR>
2018-08-15 03:57:54 +0300

    Proje Bitirildi.

---
+++ Doviztakipsystem/Doviztakipsystem/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Doviztakipsystem.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <link href="Content/bootstrap-theme.min.css" rel="stylesheet" />
+    <script src="Scripts/bootstrap.min.js"></script>
+    <script src="Scripts/jquery-1.9.0.min.js"></script>
+    <title>Döviz Takip System VER.1</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="jumbotron">
+            <h1>Döviz Kur Sistemi</h1>
+            <img src="img/SEUR.png" />
+            <img src="img/SGBP.png" />
+            <img src="img/SUSD.png" />
+
+        </div>
+
+        <div class="container">
+            <table class="table">
+                <tr>
+                    <td>Dolar</td>
+                    <td>Alış
+                        <asp:Label ID="dolaral" runat="server" Text="-"></asp:Label>
+                    </td>
+                    <td>Satış
+                        <asp:Label ID="dolarsat" runat="server" Text="-"></asp:Label></td>
+                </tr>
+                <tr>
+                    <td>ingiliz Sterlin</td>
+                    <td>Alış
+                        <asp:Label ID="euroal" runat="server" Text="-"></asp:Label>
+                    </td>
+                    <td>Satış
+                        <asp:Label ID="eurosat" runat="server" Text="-"></asp:Label></td>
+                </tr>
+
+            </table>
+        </div>
+        <asp:Timer ID="Timer1" runat="server" OnTick="Timer1_Tick" Interval="4000">
+        </asp:Timer>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    </form>
+</body>
+</html>
---
+++ Doviztakipsystem/Doviztakipsystem/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Doviztakipsystem
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        HtmlAgilityPack.HtmlWeb hweb = new HtmlAgilityPack.HtmlWeb(); // agility pack yarattık
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            // [Dolar]
+            HtmlAgilityPack.HtmlDocument alis = hweb.Load("https://dovizborsa.com/doviz/dolar");
+            HtmlAgilityPack.HtmlDocument satis = hweb.Load("https://dovizborsa.com/doviz/dolar");
+
+            foreach (HtmlAgilityPack.HtmlNode item in alis.DocumentNode.SelectNodes("//span[@class='-by-']")) // span da classı by olan nesneyi çekiyoruz
+            {
+                dolaral.Text = item.InnerText.ToString();
+            }
+            foreach (HtmlAgilityPack.HtmlNode item in satis.DocumentNode.SelectNodes("//span[@class='-sl-']"))
+            {
+                dolarsat.Text = item.InnerText.ToString();
+            }
+            
+            // [EURO]
+            HtmlAgilityPack.HtmlDocument alis2 = hweb.Load("https://dovizborsa.com/doviz/ingiliz-sterlini");
+            HtmlAgilityPack.HtmlDocument satis2 = hweb.Load("https://dovizborsa.com/doviz/ingiliz-sterlini");
+
+            foreach (HtmlAgilityPack.HtmlNode item in alis2.DocumentNode.SelectNodes("//span[@class='-by-']")) // span da classı by olan nesneyi çekiyoruz
+            {
+                euroal.Text = item.InnerText.ToString();
+            }
+            foreach (HtmlAgilityPack.HtmlNode item in satis2.DocumentNode.SelectNodes("//span[@class='-sl-']"))
+            {
+                eurosat.Text = item.InnerText.ToString();
+            }
+
+
+        }
+    }
+}
